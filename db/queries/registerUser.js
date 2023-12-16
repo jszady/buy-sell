69e81c1e719a8194858e05e0e28a8fdffe2d5068
jszady <jszady99@gmail.com>
2023-12-16 11:15:52 -0500

    set up helper function for regiserUser and installed bcryped and finished post route

--- db/queries/registerUser.js
+++ db/queries/registerUser.js
@@ -1,9 +1,18 @@
-const db = require("../connection");
+const bcrypt = require("bcrypt");
+const database = require("../connection");
+const salt = 10;
 
 const addUserToDataBase = (name, email, password, city, phone_number) => {
-  const query =
-    "INSERT INTO users(name, email, password, city, hone_number) VALUES ($1, $2, $3, $4, $5)";
-  const value = [name, email, password, city, phone_number];
-
-  return db.query(query, password);
+  return bcrypt
+    .hash(password, salt)
+    .then((hash) => {
+      const query = `INSERT INTO users(name, email, password, city, phone_number) VALUES ($1, $2, $3, $4, $5)`;
+      const value = [name, email, hash, city, phone_number];
+      return database.db.query(query, value);
+    })
+    .catch((err) => {
+      console.log("There is an error in registerUser: ", err.message);
+    });
 };
+
+module.exports = { addUserToDataBase };
