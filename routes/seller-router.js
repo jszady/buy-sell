a439df325c63758d16e60aa0a421067ca35bf0ac
jszady <jszady99@gmail.com>
2023-12-17 18:55:06 -0500

    nav works on all pages when user is logged in

--- routes/seller-router.js
+++ routes/seller-router.js
@@ -1,59 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {showListingByUserID} = require('../db/displayListingConnection')
-const {changeItemToSoldInDatabase, relistItemInDatabase} = require('../db/markItemAsSoldConnection')
+const { showListingByUserID } = require("../db/displayListingConnection");
+const {
+  changeItemToSoldInDatabase,
+  relistItemInDatabase,
+} = require("../db/markItemAsSoldConnection");
 
-
-router.get('/account', (req, res) => {
+router.get("/account", (req, res) => {
   const userID = 2;
   //Populates data from listings table base on id input in URL
   showListingByUserID(userID)
     .then((listing) => {
       if (!listing) {
-        return res.status(404).send('User not logged in');
+        return res.status(404).send("User not logged in");
       }
 
       //Listing price will now display in local currency style
-      const prices = listing.price.toLocaleString()
+      const prices = listing.price.toLocaleString();
 
-      const exports = {listing: listing, price: prices}
-      res.render('seller_listings', exports);
+      const exports = {
+        listing: listing,
+        price: prices,
+        user: req.session.user,
+      };
+      res.render("seller_listings", exports);
     })
     .catch((err) => {
-      console.error(err)
-      return('User not logged in')
-    })
+      console.error(err);
+      return "User not logged in";
+    });
 });
 
-router.post('/sold', (req, res) => {
-
+router.post("/sold", (req, res) => {
   const listingId = req.body.id;
   changeItemToSoldInDatabase(listingId)
     .then((message) => {
       console.log(message);
-      res.redirect('account');
+      res.redirect("account");
     })
     .catch((err) => {
-      console.error(err)
-      return(err)
-    })
+      console.error(err);
+      return err;
+    });
+});
 
-})
-
-router.post('/relist', (req, res) => {
-
+router.post("/relist", (req, res) => {
   const listingId = req.body.id;
   relistItemInDatabase(listingId)
     .then((message) => {
       console.log(message);
-      res.redirect('account');
+      res.redirect("account");
     })
     .catch((err) => {
-      console.error(err)
-      return(err)
-    })
-
-})
-
+      console.error(err);
+      return err;
+    });
+});
 
 module.exports = router;
