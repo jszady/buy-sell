df29f782a28244c49f9f278baa2ec16df4365ed6
Jill Regan <jillian.jnr@hotmail.com>
2023-12-16 21:49:04 -0500

    Cleaned up code in listings-router.js

--- public/scripts/counter.js
+++ public/scripts/counter.js
@@ -1,17 +1,12 @@
-// Function that will count the maount of characters in the tweet text area.
+// Function that will count the amount of characters in the  text area.
 const countTweetCharacters = function() {
 
-  //Sets the variable maxCharacterCount to equal the number displayed on the HTML form underneath the tweet form
   let maxCharacterCount = document.getElementById('max-count');
-  //Sets the variable currentCharacterCount to the length of the text in the tweet form
   let currentCharacterCount = document.getElementById('message').value.length;
 
-  //Sets result to equal the max character count subtract the current character count
-  let result = 140 - currentCharacterCount;
-  //Sets the maxCharacterCount as the HTML number
+  let result = 120 - currentCharacterCount;
   maxCharacterCount.innerHTML = result;
 
-  //Checks to see if the count needs to turn red
   if(result < 0){
     maxCharacterCount.style.color = 'red';
   } else{
@@ -20,7 +15,6 @@ const countTweetCharacters = function() {
 }
 
 $(document).ready(function() {
-
 
   //On any input to the submit tweet form it will execute the countTweetCharacter function
   $('.user-message-textarea').on('input', function(){
--- routes/listings-router.js
+++ routes/listings-router.js
@@ -11,11 +11,14 @@ const client = require('twilio')(accountSid, authToken);
 router.get('/:id', (req, res) => {
   const listingId = req.params.id;
 
+  //Populates data from listings table base on id input in URL
   showListingByID(listingId)
     .then((listing) => {
       if (!listing) {
         return res.status(404).send('Listing not available');
       }
+
+      //Listing price will now display in local currency style
       const prices = listing.price.toLocaleString()
 
       const exports = {listing: listing, price: prices}
@@ -30,11 +33,12 @@ router.get('/:id', (req, res) => {
 
 
 router.post('/sms', (req, res) => {
+  //Creates the message based on user input. Combines their message and their phone number
   const userMessage = `${req.body.text}. My phone number is ${req.body.userNumber}.`;
+  //Gets the phone number of the listing owner
   const adminPhoneNumber = req.body.number;
 
-
-
+  //Errror handling
   if(!userMessage) {
     return res.status(404).send('Must enter message text.')
   }
@@ -46,6 +50,8 @@ router.post('/sms', (req, res) => {
   if(userMessage.length > 153) {
     return res.status(404).send('Message must bet 120 characters or under.')
   }
+
+  //Sends Twilio Message
   client.messages
     .create({
       body: userMessage,
@@ -56,7 +62,6 @@ router.post('/sms', (req, res) => {
     console.log(message.sid)
     res.render('delivered')});
    })
-
 
 });
 
