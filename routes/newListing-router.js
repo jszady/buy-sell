3296664d0e2bf7b5e19576ece3bc713b35d00f41
YoasterToaster <joefoschia@gmail.com>
2023-12-17 20:59:35 -0500

    modified newListingConnection.js to upload images, started on main page html/css, and added index.css

--- routes/newListing-router.js
+++ routes/newListing-router.js
@@ -10,13 +10,14 @@ router.post("/newlisting", (req, res) => {
 
     // Still need to get the cookie data for the user_id
     const data = {
+      image: req.body.image,
       brand: req.body.brand,
       make: req.body.make,
       year: req.body.year,
       color: req.body.color,
       transmission: req.body.transmission,
       price: req.body.price,
-      description: req.body.description,
+      description: req.body.description
     };
     console.log(data);
     insertListing(data).then((response) => {
