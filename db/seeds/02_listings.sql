672a7f9dce37157852dde703e981f36573e9f0d1
Jill Regan <jillian.jnr@hotmail.com>
2023-12-18 10:55:37 -0500

    Accidenlty pushed test data. Deleted and now repushing

--- db/seeds/02_listings.sql
+++ db/seeds/02_listings.sql
@@ -5,9 +5,3 @@ VALUES
   ('https://www.autolist.com/6tuem73u73an/5V6WX0Ct4lGIEQ9Tsx3PcD/74bace2c9d217889262bd7ca25451561/2018-toyota-corolla-1166-image.jpg', 2, 'Toyota', 'Corolla', '2018', 'Blue', 'Manual', 18000, 'Well-maintained with service history'),
   ('https://i.ytimg.com/vi/eFWUQc-wDzI/maxresdefault.jpg', 3, 'Ford', 'Mustang', '2022', 'Black', 'Automatic', 30000, 'Brand new, never used');
 
-
-
-
-('https://images.trailbuiltoffroad.com/web-compressed/1363872-1-2000-ranger-ford-wulf-s-suspension-lift-3in-black-rock-d-widow-black.jpg', 4, 'Ford', 'Ranger', '2000', 'Black', 'Manual', 9000, 'Sold as is. Still runs great.');
-('https://uploads.builtforbackroads.com/uploads/2022/12/2022.12.01-SUBARU-FORESTER-2005_1.jpg', 4, 'Subaru', 'Forester', '2006', 'Red', 'Automatic', 5000, 'Needs some work.');
-('https://i.pinimg.com/474x/4b/6c/13/4b6c13a26d33c15ad5336da14c45c4ac.jpg', 4, 'Mazda', 'MAZDA3', '2005', 'Blue', 'Manual', 4500, 'Reliable vehicle with low kms');
