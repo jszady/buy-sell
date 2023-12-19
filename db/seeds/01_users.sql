e9408cd5b1320f4bd2918d2d7093952b5ebf8d13
jszady <jszady99@gmail.com>
2023-12-19 13:52:05 -0500

    updated DB

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,5 +3,5 @@ INSERT INTO users (name, email, password, city, phone_number)
 VALUES
   ('John Doe', 'john.doe@example.com', 'securepass123', 'Vancouver', '+17053054055'),
   ('Jane Smith', 'jane.smith@example.com', 'mypassword456', 'Toronto', '987-654-3210'),
-  ('Bob Johnson', 'bob.johnson@example.com', 'strongpass789', 'Vancouver', '555-123-4567');
-  ('Jason Brown', "j.brown@gmail.com", 'theStrongestPassword1238',"France","991-887-5823");
+  ('Bob Johnson', 'bob.johnson@example.com', 'strongpass789', 'Vancouver', '555-123-4567'),
+  ('Jason Brown', 'j.brown@gmail.com', 'theStrongestPassword1238','France', '911-887-5853');
