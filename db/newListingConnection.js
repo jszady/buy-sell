78e50565678e1c8deef7793954273dd08f0c622c
Jill Regan <jillian.jnr@hotmail.com>
2023-12-19 14:17:56 -0500

    Update create listings router and functions to include 2 more images

--- db/newListingConnection.js
+++ db/newListingConnection.js
@@ -2,15 +2,17 @@ const { db } = require("./connection");
 
 
 // This function inserts a new row into the listings table depending on what the user entered
-const insertListing = (listingInfo) => {
+const insertListing = (listingInfo, userId) => {
   return db
     .query(
-      `INSERT INTO listings (thumbnail_photo_url, users_id, brand, make, year, color, transmission, price, description)
+      `INSERT INTO listings (thumbnail_photo_url, image_2, image_3, users_id, brand, make, year, color, transmission, price, description)
     VALUES
-      ($1, $2, $3, $4, $5, $6, $7, $8, $9);`,
+      ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`,
       [
         listingInfo.image,
-        3, // user_id will need to be included in the top from the logged in user data
+        listingInfo.image2,
+        listingInfo.image3,
+        userId, // user_id will need to be included in the top from the logged in user data
         listingInfo.brand,
         listingInfo.make,
         listingInfo.year,
