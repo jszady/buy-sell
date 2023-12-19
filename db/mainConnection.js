c61c7d741418bb8d5802aafe3776731ca6985f90
jszady <jszady99@gmail.com>
2023-12-19 17:09:27 -0500

    created the featured tab changed index.ejs and mainConnection.js

--- db/mainConnection.js
+++ db/mainConnection.js
@@ -5,30 +5,36 @@ const getListing = (filter) => {
   // Declaring the parameters for the statement, the conditions to be joined together later and the querystring that will hold the finished statement
   const queryParams = [];
   const conditions = [];
-  let queryString = 'SELECT id, thumbnail_photo_url, year, brand, color, price, description FROM listings';
-  
+  let queryString =
+    "SELECT id, thumbnail_photo_url, year, brand, color, price, description, featured FROM listings";
+
   // If any of these exist they get added to the parameters and the condition statemetn at the end
-  if (filter.brand){
+
+  if (filter.brand) {
     queryParams.push(`%${filter.brand}%`);
     conditions.push(`brand LIKE $${queryParams.length}`);
-  } 
+  }
 
-  if (filter.minPrice){
+  if (filter.minPrice) {
     queryParams.push(`${filter.minPrice}`);
     conditions.push(`price >= $${queryParams.length}`);
   }
 
-  if (filter.maxPrice){
+  if (filter.maxPrice) {
     queryParams.push(`${filter.maxPrice}`);
     conditions.push(`price <= $${queryParams.length}`);
+  }
+  if (filter.isFeatured) {
+    conditions.push(`featured = TRUE`);
   }
 
   // Makes it so that no matter what option is left out or selected there will always be a 'WHERE' at the start and an 'AND' in between
   if (conditions.length > 0) {
-    queryString += ' WHERE ' + conditions.join(' AND ') + ';';
+    queryString += " WHERE " + conditions.join(" AND ") + ";";
   }
-  
-  return db.query(queryString,queryParams)
+
+  return db
+    .query(queryString, queryParams)
     .then((result) => {
       const user = result.rows;
       return user || null;
@@ -41,17 +47,14 @@ const getListing = (filter) => {
 // Returns the data in an object for all the listings
 const getAllListings = () => {
   return db
-  .query(
-    `SELECT * FROM listings;`,
-    []
-  )
-  .then((result) => {
-    const user = result.rows;
-    return user || null;
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .query(`SELECT * FROM listings;`, [])
+    .then((result) => {
+      const user = result.rows;
+      return user || null;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
-module.exports = {getListing, getAllListings};
+module.exports = { getListing, getAllListings };
